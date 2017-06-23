76b77e6f61080507ee32bdf79b62c95d19f9be7c
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-06-22 22:00:44 -0400

    finish main package, add unit tests with test cases (and they mostly work, except for 3 of the edge cases

--- goosrelease.go
+++ goosrelease.go
@@ -2,8 +2,9 @@ package goosrelease
 
 import (
 	"bufio"
-	"fmt"
+	"errors"
 	"os"
+	"strings"
 )
 
 const EtcOsRelease string = "/etc/os-release"
@@ -25,12 +26,59 @@ type OsRelease struct {
 
 func Read(filename string) (osrelease map[string]string, err error) {
 	osrelease = make(map[string]string)
+	err = nil
 
 	lines, err := readFile(filename)
-	for _, v := range lines {
-		fmt.Print("%s\n", v)
+	if err != nil {
+		return
 	}
-	return osrelease, nil
+
+	for _, v := range lines {
+		key, value, err := parseLine(v)
+		if err == nil {
+			osrelease[key] = value
+		}
+	}
+	return
+}
+
+func parseLine(line string) (key string, value string, err error) {
+	err = nil
+
+	if len(line) == 0 {
+		err = errors.New("Skipping: zero-length")
+		return
+	}
+	if line[0] == '#' {
+		err = errors.New("Skipping: comment")
+		return
+	}
+
+	splitString := strings.SplitN(line, "=", 2)
+	if len(splitString) != 2 {
+		err = errors.New("Can not extract key=value")
+		return
+	}
+
+	key = splitString[0]
+	key = strings.Trim(key, " ")
+
+	value = splitString[1]
+	value = strings.Trim(value, " ")
+
+	if strings.ContainsAny(value, `"`) {
+		first := string(value[0:1])
+		last := string(value[len(value)-1:])
+
+		if first == last && strings.ContainsAny(first, `"'`) {
+			value = strings.Trim(value, `"'`)
+		}
+		value = strings.Replace(value, `\"`, `"`, -1)
+	}
+	value = strings.Replace(value, `\$`, `$`, -1)
+	value = strings.Replace(value, `\\`, `\`, -1)
+	value = strings.Replace(value, "\\`", "`", -1)
+	return
 }
 
 func readFile(filename string) (lines []string, err error) {
--- goosrelease_test.go
+++ goosrelease_test.go
@@ -1,6 +1,80 @@
 package goosrelease
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestReadFile(*testing.T) {
+func TestReadNoFile(t *testing.T) {
+	_, err := Read("test/nosuchfile")
+	if err == nil {
+		t.Errorf("Read() on non-existant file returned nil, should not be nil")
+	}
+}
+
+func TestRead(t *testing.T) {
+	expectedResults := map[int]map[string]string{
+		1: {
+			"NAME":        "void",
+			"ID":          "void",
+			"DISTRIB_ID":  "void",
+			"PRETTY_NAME": "void",
+		},
+		2: {},
+		3: {
+			"NAME":           "CentOS Linux",
+			"VERSION":        "7 (Core)",
+			"ID":             "centos",
+			"ID_LIKE":        "rhel fedora",
+			"VERSION_ID":     "7",
+			"PRETTY_NAME":    "CentOS Linux 7 (Core)",
+			"ANSI_COLOR":     "0;31",
+			"CPE_NAME":       "cpe:/o:centos:centos:7",
+			"HOME_URL":       "https://www.centos.org/",
+			"BUG_REPORT_URL": "https://bugs.centos.org/",
+		},
+		4: {
+			"NAME":           "CoreOS",
+			"ID":             "coreos",
+			"VERSION":        "1185.3.0",
+			"VERSION_ID":     "1185.3.0",
+			"BUILD_ID":       "2016-11-01-0605",
+			"PRETTY_NAME":    "CoreOS 1185.3.0 (MoreOS)",
+			"ANSI_COLOR":     "1;32",
+			"HOME_URL":       "https://coreos.com/",
+			"BUG_REPORT_URL": "https://github.com/coreos/bugs/issues",
+		},
+		5: {
+			"NAME":           "Container Linux by CoreOS",
+			"ID":             "coreos",
+			"VERSION":        "1235.6.0",
+			"VERSION_ID":     "1235.6.0",
+			"BUILD_ID":       "2017-01-10-0545",
+			"PRETTY_NAME":    "Container Linux by CoreOS 1235.6.0 (Ladybug)",
+			"ANSI_COLOR":     "38;5;75",
+			"HOME_URL":       "https://coreos.com/",
+			"BUG_REPORT_URL": "https://github.com/coreos/bugs/issues",
+		},
+		6: {
+			"NAME":        "dummy",
+			"DISTRIB_ID":  "foobar",
+			"VERSION":     "17",
+			"ID":          ",;: ",
+			"PRETTY_NAME": "\\$ \\` \\ \"",
+		},
+	}
+
+	for test := 1; test <= len(expectedResults); test++ {
+		filename := "test/os-release." + strconv.Itoa(test)
+		osrelease, err := Read(filename)
+		if err != nil {
+			t.Fatalf("Error reading test file '%v': %v", filename, err)
+		} else {
+			for key, value := range expectedResults[test] {
+				if osrelease[key] != value {
+					t.Errorf("In file 'test/os-release.%v', Read() returned '%v' = '%v', should be '%v'", test, key, osrelease[key], value)
+				}
+			}
+		}
+	}
 }
